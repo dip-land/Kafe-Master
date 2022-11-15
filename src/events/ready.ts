1c3e33aa0521a4ae5b0a2e4adec4832a4ee1b6bb
shhh7612 <82669765+shhh7612@users.noreply.github.com>
2022-11-15 08:51:44 -0600

    Grafana test

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,4 +1,5 @@
 import { type Client, Routes } from 'discord.js';
+import { start } from '../handlers/grafana/start.js';
 import { check } from '../handlers/database/index.js';
 import { beta, commands } from '../index.js';
 
@@ -7,6 +8,7 @@ export const once = true;
 export default async (client: Client<boolean>) => {
 	console.log(client.user?.username, 'online');
 	check();
+	start();
 	//client.guilds.fetch(beta ? '981639333549322262' : '632717913169854495').then((g) => registerGuild(g));
 	client.rest
 		.put(Routes.applicationGuildCommands(beta ? '996196343120928859' : '995370187626905611', beta ? '981639333549322262' : '632717913169854495'), { body: commands })
