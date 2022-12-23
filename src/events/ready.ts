d744444e6a134a1353d7f139c71d7dbec8bdfc62
shhh7612 <82669765+shhh7612@users.noreply.github.com>
2022-12-23 16:29:48 -0600

    Split
    Split kafe-master into helpful-myu and naughty-myu

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,5 @@
 import { type Client, Routes } from 'discord.js';
-import { check } from '../handlers/database/index.js';
+import { startDatabase } from '../handlers/database/index.js';
 import { beta, commands } from '../index.js';
 import { Event } from '../structures/event.js';
 
@@ -8,12 +8,13 @@ export default new Event({
 	on: false,
 	async fn(client: Client<boolean>) {
 		console.log(client.user?.username, 'online');
-		check();
+		startDatabase();
 		//client.guilds.fetch(beta ? '981639333549322262' : '632717913169854495').then((g) => registerGuild(g));
 		if (beta) {
 			client.rest.put(Routes.applicationGuildCommands('996196343120928859', '981639333549322262'), { body: commands }).catch(console.error);
 		} else {
 			client.rest.put(Routes.applicationCommands('995370187626905611'), { body: commands }).catch(console.error);
+			client.rest.put(Routes.applicationCommands('1055960319161282630'), { body: commands }).catch(console.error);
 		}
 	},
 });
